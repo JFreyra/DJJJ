b575a4dbe70a308561a701e10cebf5473cf41953
James Wang <jameswang9909@hotmail.com>
2017-04-04 15:41:59 -0400

    Add changing background

--- static/js/home.js
+++ static/js/home.js
@@ -4,16 +4,20 @@
 var daylight = [];
 var getData = function() {
     $.ajax({
-	url: '/dataret',
-	type: 'GET',
-	success: function( d ) {
-	    d = JSON.parse(d);
-	    daylight = d['data'];
-	    createGraph(daylight)
-	}
+        url: '/dataret',
+        type: 'GET',
+        success: function( d ) {
+            d = JSON.parse(d);
+            daylight = d['data'];
+            createGraph(daylight)
+        }
     });
 
 };
+
+var night = "#001848";
+var orange = "orange";
+var day = "#87CEEB";
 
 // Onload
 // inits data on window load
@@ -22,118 +26,128 @@ window.onload = function(){
     console.log("loaded");
 };
 
-var createGraph = function(daylight){
-    console.log(daylight);
+function GradientReader(colorStops) {
 
-    var createGradient = function(svg, id, stops){
-	var svgNS = svg.namespaceURI;
-	var grad  = document.createElementNS(svgNS,'linearGradient');
-	grad.setAttribute("id",id);
-	for (var i = 0; i< stops.length; i++) {
-            var attrs = stops[i];
-            var stop = document.createElementNS(svgNS,'stop');
-            for (var attr in attrs) {
-		if (attrs.hasOwnProperty(attr)) stop.setAttribute(attr,attrs[attr]);
-            }
-            grad.appendChild(stop);
-	}
+    var canvas = document.createElement('canvas'),     // create canvas element
+        ctx = canvas.getContext('2d'),                 // get context
+        gr = ctx.createLinearGradient(0, 0, 101, 0),   // create a gradient
+        i = 0, cs;
 
-	var defs = svg.querySelector('defs') ||
-            svg.insertBefore( document.createElementNS(svgNS,'defs'), svg.firstChild);
-	return defs.appendChild(grad);
+    canvas.width = 101;                                // 101 pixels incl.
+    canvas.height = 1;                                 // as the gradient
+
+    for(; cs = colorStops[i++];)                       // add color stops
+        gr.addColorStop(cs.stop, cs.color);
+
+    ctx.fillStyle = gr;                                // set as fill style
+    ctx.fillRect(0, 0, 101, 1);                        // draw a single line
+
+    // method to get color of gradient at % position [0, 100]
+    this.getColor = function(pst) {
+        return ctx.getImageData(pst|0, 0, 1, 1).data;
     };
+}
 
-    var night = "#001848";
-    var orange = "orange";
-    var day = "#87CEEB";
+var gr = new GradientReader([
+    {stop:0.2, color:night},
+    {stop:0.35, color:orange},
+    {stop:0.45, color:day},
+    {stop:0.8, color:orange},
+    {stop:1.0,color:night}
+]);
 
-    // Dynamically set this
-    createGradient(document.getElementById("graphSVG"),"background",[
-	{offset:'0%', 'stop-color':night},
-	{offset:'30%', 'stop-color':orange},
-	{offset:'45%', 'stop-color':day},
-	{offset:'80%', 'stop-color':orange},
-	{offset:'100%','stop-color':night}
-    ]);
+var i = 0;
+var setBackground = function() {
+    setTimeout(function() {
+        var color = gr.getColor(Math.floor(i*100/364));
+        color = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
+        background.attr("fill", color);
+        i++;
+        if (i < 365) {
+            setBackground();
+        }
+    }, 50);
+};
 
-    d3.select('svg rect').attr('fill','url(#background)');
+var background = d3.select("svg rect");
 
+var createGraph = function(daylight){
     var height = 700;
     var width = 1000;
     var barPadding = 0;
     var barWidth = (width / daylight.length*1.5) - barPadding;
 
     var yScale = d3.scale.linear()
-	.domain([0, d3.max(daylight)])
-	.range([0, height/1.75]);
+        .domain([0, d3.max(daylight)])
+        .range([0, height/1.75]);
 
 
     var xScale = d3.scale.ordinal()
-	.domain(daylight)
-	.rangeBands([0, width], 0, 0);
+        .domain(daylight)
+        .rangeBands([0, width], 0, 0);
 
     var svg = d3.select("#graphSVG")
-	.style('width', width + 'px')
-	.style('height', height + 'px');
+        .style('width', width + 'px')
+        .style('height', height + 'px');
 
     svg.selectAll('rect')
-	.data(daylight)
-	.enter()
-	.append('rect')
-	.attr('class', 'bar')
-	.attr("x", function (d, i) {
+        .data(daylight)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr("x", function (d, i) {
             return xScale(d);
-	})
-	.attr("y", function (d, i) {
+        })
+        .attr("y", function (d, i) {
             return height;
-	})
-	.attr("width", function (d, i) {
+        })
+        .attr("width", function (d, i) {
             return xScale.rangeBand()
-	})
-	.attr("fill", function (d, i) {
+        })
+        .attr("fill", function (d, i) {
             return 'rgb(0, ' + (182-Math.abs(i-182)) + ', 0)'
-	})
-	.attr("height", 0)
-	.transition()
-	.duration(200)
-	.delay(function (d, i) {
+        })
+        .attr("height", 0)
+        .transition()
+        .duration(200)
+        .delay(function (d, i) {
             return i * 50;
-	})
-	.attr("y", function (d, i) {
+        })
+        .attr("y", function (d, i) {
             return height - yScale(d);
-	})
-	.attr("height", function (d, i) {
+        })
+        .attr("height", function (d, i) {
             return yScale(d);
-	});
-
+        });
+    setBackground();
 };
 
 //Generate y-axis label
 var yAxisLabel = function(){
     var hours = [];
     for(i=1;i<25;i++){
-	var h = {};
-	if(i<10){
-	    h.cx = "2.5";
-	}else{
-	    h.cx = "-1";
-	};
-	h.cy = (715.0/24)*(25-i)-((715.0/24)/2);
-	h.t = i;
-	h.color = parseInt(255-i*(255/24));
-	hours.push(h);
+        var h = {};
+        if(i<10){
+            h.cx = "2.5";
+        }else{
+            h.cx = "-1";
+        };
+        h.cy = (715.0/24)*(25-i)-((715.0/24)/2);
+        h.t = i;
+        h.color = parseInt(255-i*(255/24));
+        hours.push(h);
     };
     console.log(hours);
     var text = d3.select("#axis").selectAll("g")
-	.data(hours)
-	.enter()
-	.append("text");
+        .data(hours)
+        .enter()
+        .append("text");
     console.log(text);
     var addText = text
-	.attr("x",function(d){return d.cx;})
-	.attr("y",function(d){return d.cy;})
-	.attr("fill",function(d){return "rgb("+d.color+","+d.color+","+d.color+")"})
-	.text(function(d){return d.t;})
-	.attr("font-size","15px");
+        .attr("x",function(d){return d.cx;})
+        .attr("y",function(d){return d.cy;})
+        .attr("fill",function(d){return "rgb("+d.color+","+d.color+","+d.color+")"})
+        .text(function(d){return d.t;})
+        .attr("font-size","15px");
 };
 yAxisLabel();
